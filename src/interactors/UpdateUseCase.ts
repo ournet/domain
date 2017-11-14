1f588bead6099e7b404eded905617bcf2c41f44a
Dumitru Cantea <canteadumitru@gmail.com>
2017-11-14 22:57:49 +0200

    no use case Validator

--- src/interactors/UpdateUseCase.ts
+++ src/interactors/UpdateUseCase.ts
@@ -1,24 +1,15 @@
 
 import { UseCase } from './UseCase';
 import { IRepository, RepUpdateOptions, RepUpdateData } from './repository';
-import { IValidator } from '../Validator';
 
 export class UpdateUseCase<ID, ENTITY> extends UseCase<RepUpdateData<ENTITY>, ENTITY, RepUpdateOptions<ENTITY>>{
 
-    constructor(protected repository: IRepository<ID, ENTITY>, protected validator: IValidator<ENTITY>) {
+    constructor(protected repository: IRepository<ID, ENTITY>) {
         super();
     }
 
     protected innerExecute(data: RepUpdateData<ENTITY>, options?: RepUpdateOptions<ENTITY>) {
         return this.repository.update(data, options);
-    }
-
-    protected validateData(data: RepUpdateData<ENTITY>): Promise<RepUpdateData<ENTITY>> {
-        try {
-            this.validator.update(data)
-        } catch (e) {
-            return Promise.reject(e);
-        }
     }
 
 }
