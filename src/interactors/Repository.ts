454b1712cd8f5219d674128212bb9e65920fc931
Dumitru Cantea <canteadumitru@gmail.com>
2017-11-14 06:53:44 +0200

    IUseCase, IRepository

---
+++ src/interactors/Repository.ts
@@ -0,0 +1,42 @@
+
+// import { IDictionary } from '../utils';
+// import { DataKeys } from './data-keys';
+
+export interface RepAccessOptions<T> {
+    /**
+     * Fields to return separated by spaces
+     */
+    fields?: (keyof T)[]
+}
+
+export interface RepUpdateOptions<T> extends RepAccessOptions<T> {
+
+}
+
+export interface RepUpdateData<ID, T> {
+    id: ID
+    update?: T
+    delete?: (keyof T)[]
+    // inc?: { [index: string]: number }
+}
+
+// export type RepListData = {
+//     keys: DataKeys
+//     count: number
+// }
+
+// export interface RepGetData extends DataKeys {
+
+// }
+
+export interface IRepository<ID, T> {
+    delete(id: ID): Promise<boolean>
+    getById(id: ID, options?: RepAccessOptions<T>): Promise<T>
+    exists(id: ID): Promise<boolean>
+    // count(data?: RepGetData): Promise<number>
+    create(data: T, options?: RepAccessOptions<T>): Promise<T>
+    update(data: RepUpdateData<ID, T>, options?: RepUpdateOptions<T>): Promise<T>
+    // one(data: RepGetData, options?: RepAccessOptions): Promise<T>
+    // list(data: RepListData, options?: RepAccessOptions): Promise<T[]>
+}
+
