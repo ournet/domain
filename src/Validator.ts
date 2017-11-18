05863eff45a4f48013427d4dab5f674f246d4e69
Dumitru Cantea <canteadumitru@gmail.com>
2017-11-18 16:04:32 +0200

    validate RepUpdateData

--- src/Validator.ts
+++ src/Validator.ts
@@ -8,47 +8,57 @@ export interface IValidator<T> {
     update(data: RepUpdateData<T>, options?: any): RepUpdateData<T>
 }
 
-export class Validator<T> implements IValidator<T> {
-    private createSchema: ObjectSchema
-    private updateSchema: ObjectSchema
+export interface ValidatorDataItem {
+    schema?: any
+    // fields?: string[]
+}
 
-    constructor(createSchema?: any, updateSchema?: any) {
-        if (createSchema) {
-            this.createSchema = <ObjectSchema>createSchema;
-        }
-        if (updateSchema) {
-            this.updateSchema = <ObjectSchema>updateSchema;
-        }
-    }
+export class Validator<T> implements IValidator<T> {
+
+    constructor(private createData?: ValidatorDataItem, private updateData?: ValidatorDataItem) { }
 
     create(data: T, options?: any): T {
-        if (!data) {
-            throw new DataValidationError(`'data' is required`);
-        }
-
-        if (this.createSchema) {
-            return validate(this.createSchema, data, options);
-        }
-        return data;
+        return this.validate(this.createData, data, options);
     }
 
     update(data: RepUpdateData<T>, options?: any): RepUpdateData<T> {
         if (!data) {
             throw new DataValidationError(`'data' is required`);
         }
-        if (!data.item) {
-            throw new DataValidationError(`'data.item' is required`);
+        // if (!data.item) {
+        //     throw new DataValidationError(`'data.item' is required`);
+        // }
+
+        data = this.validate(this.updateData, data, options);
+
+        return data;
+    }
+
+    private validate<DT>(itemData: ValidatorDataItem, data: DT, options?: ValidationOptions): DT {
+        if (!data) {
+            throw new DataValidationError(`'data' is required`);
         }
 
-        if (this.updateSchema) {
-            data.item = validate(this.updateSchema, data.item, options);
+        if (itemData) {
+            // if (itemData.fields && itemData.fields.length) {
+            //     Object.keys(data).forEach(key => itemData.fields.indexOf(key) < 0 && delete data[key]);
+            // }
+            if (itemData.schema) {
+                return validate(itemData.schema, data, options);
+            }
         }
         return data;
     }
 }
 
 function validate<T>(schema: ObjectSchema, data: T, options?: ValidationOptions): T {
-    options = options || { abortEarly: true, convert: true, allowUnknown: false, stripUnknown: false };
+    options = Object.assign({
+        abortEarly: true,
+        convert: true,
+        allowUnknown: true,
+        stripUnknown: false
+    },
+        options || {});
 
     const result = schema.validate(data, options);
     if (result.error) {
