08de5d4a40eb6f593c3d292833008c6ece2a8ed8
Dumitru Cantea <canteadumitru@gmail.com>
2018-08-12 07:59:29 +0300

    joi EntityValidator

--- src/entity-validator.ts
+++ src/entity-validator.ts
@@ -1,7 +1,39 @@
 import { BaseEntity } from "./entities";
 import { RepositoryUpdateData } from "./repository";
+import { validate as joiSchemaValidate, SchemaMap } from 'joi';
 
-export interface EntityValidator<T extends BaseEntity> {
-    onCreate(data: T): Promise<T>
-    onUpdate(data: RepositoryUpdateData<T>): Promise<RepositoryUpdateData<T>>
+export interface EntityValidatorOptions {
+    createSchema: SchemaMap
+    updateSchema: SchemaMap
+}
+
+export class EntityValidator<T extends BaseEntity> {
+    constructor(private options: EntityValidatorOptions) { }
+
+    onCreate(data: T) {
+        const result = validateSchema(this.options.createSchema, data);
+        if (result.error) {
+            throw result.error;
+        }
+        return result.value;
+    }
+    onUpdate(data: RepositoryUpdateData<T>) {
+        const result = validateSchema(this.options.updateSchema, data);
+        if (result.error) {
+            throw result.error;
+        }
+        return result.value;
+    }
+}
+
+function validateSchema<T>(schema: SchemaMap, data: T) {
+    return joiSchemaValidate<T>(data, schema, {
+        allowUnknown: false,
+        abortEarly: true,
+        convert: true,
+        noDefaults: false,
+        presence: 'optional',
+        stripUnknown: false,
+        skipFunctions: false,
+    });
 }
